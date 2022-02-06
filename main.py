7d7af17f92dbc8cea86d0dc029f3c9e200827623
Lucas <77511972+lucaslaya@users.noreply.github.com>
2022-02-06 01:26:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,17 @@
 # Upcoming tourneys, completed tourneys
 # Rankings, stats
 
+from nis import match
 import flask
 from flask import request, jsonify, Flask
-from mainScraping import Vlr
+from vlrScraping import Vlr
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['JSON_SORT_KEYS'] = False
 
-VLR = Vlr()
+base_vlr = Vlr()
 
 # Base Endpoint
 @app.route('/', methods=['GET'])
@@ -22,7 +25,8 @@ def home():
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 # Match data Endpoint
-@app.route('/api/v1/match', methods=['GET'])
+# DONE
+@app.route('/match', methods=['GET'])
 def api_match():
     # Check for id in url
     if 'id' in request.args:
@@ -30,14 +34,80 @@ def api_match():
     else:
         return "Error: No id field provided. Please specify an id.", 404
 
-    return {"data": id}, 200
+    (date,time,patch,tourney,tourney_round,tourney_icon,tourney_url,match_format,
+    status,notes,team1,team1_elo,team1_url,team1_score,team2,team2_elo,team2_url,
+    team2_score,team1_icon,team2_icon,streams,vods,overview) = base_vlr.match(id)
+    
+    data = {
+        "match_info": {
+            "date": date,
+            "time": time,
+            "patch": patch,
+            "tourney": tourney,
+            "tourney_round": tourney_round,
+            "tourney_icon": tourney_icon,
+            "tourney_url": tourney_url,
+            "format": match_format,
+            "status": status,
+            "notes": notes,
+            "team1": team1,
+            "team1_elo": team1_elo,
+            "team1_url": team1_url,
+            "team1_score": team1_score,
+            "team2": team2,
+            "team2_elo": team2_elo,
+            "team2_url": team2_url,
+            "team2_score": team2_score,
+            "team1_icon": team1_icon,
+            "team2_icon": team2_icon,
+            "streams": streams,         #List
+            "vods": vods                #List
+        },
+        "match_stats": {
+            "overview": overview,       #List
+#            "performance": performance, #List
+#            "economy": economy          #List
+        },
+#        "comments": comments            #List
+    }
+
+    return jsonify({"match": data}), 200
+
+# Team Endpoint
+@app.route('/team', methods=['GET'])
+def api_team():
+    # Check for id in url
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id.", 404
+    
+    # Check for info in url
+    if 'info' in request.args:
+        info = str(request.args['info'])
+    else:
+        info = "overview"
+
+    (name, tag, website, twitter, country, flag, logo, match_data) = base_vlr.team(id, info)
+
+    data = {
+        "name": name,
+        "tag": tag,
+        "website": website,
+        "twitter": twitter,
+        "country": country,
+        "flag": flag,
+        "logo": logo,
+        "match_data": match_data    # List
+    }
+    
+    return jsonify({"team": data})
 
 # Upcoming matches Endpoint
-@app.route('/api/v1/upcoming/match', methods=['GET'])
+@app.route('/upcoming/match', methods=['GET'])
 def api_upcomingMatch():
-    matches = VLR.vlr_upcoming()
-    data = {"matches": matches}
+    
+    return jsonify({"upcoming": "upcoming endpoint"})
 
-    return {"data": data}, 200
 
 app.run()
